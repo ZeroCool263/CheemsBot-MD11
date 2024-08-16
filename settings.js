06011875d10550e45b0eefe770f4572c948257b6
†ZeroCool† <herocool263@gmail.com>
2024-08-16 23:37:54 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,18 +20,18 @@ global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.ownernumber = '263712473551' //ur owner number
+global.ownername = 'Dark ZeroCool' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
+global.wagc = "https://whatsapp.com/channel/0029VaZypgoIyPtaodVKp61o"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "ZeroCool\n\n+263712473551"
+global.creator = "263712473551@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["263712473551"] // Premium User
 global.hituet = 0
 
 //bot sett
